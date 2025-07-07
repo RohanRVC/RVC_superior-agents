9a60f9ea9a32a9a93994680e818f501f800872bd
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-07-08 02:02:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# import sys
+# import os
+# sys.path.append(os.path.join(os.path.dirname(__file__), "agent"))
+
+from agent.scripts.autofounder import starter_prompt
+
+
+
+
+if __name__ == "__main__":
+    # modify this if you want to run this forever
+	print('Starting starter_prompt.........................................')	
+	starter_prompt()
+
