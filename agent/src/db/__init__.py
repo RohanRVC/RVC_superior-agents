788663e6154ca777b798c06da68dd9bb7d45a52a
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-07-08 23:47:19 +0530

    Update __init__.py

--- agent/src/db/__init__.py
+++ agent/src/db/__init__.py
@@ -1,5 +1,17 @@
-from src.db.interface import DBInterface
-from src.db.rest_api import APIDB
-from src.db.sqlite import SQLiteDB
+from agent.src.db.chat import ChatLogger#, save_chat_history, get_chat_by_role
+from agent.src.db.steps import StepLogger#, save_last_step, resume_session
+from agent.src.db.export import export_as_json, export_as_md
+import os
 
-__all__ = ["DBInterface", "APIDB", "SQLiteDB"]
+class SQLiteDB(ChatLogger,StepLogger):
+    def __init__(self, db_path="logger/autofounder.db"):
+        os.makedirs(os.path.dirname(db_path), exist_ok=True)  # ✅ ensure folder exists
+        print(f"🔧 Initializing SQLiteDB at {db_path}")  # ✅ Add this debug line
+        StepLogger.__init__(self, db_path)
+        ChatLogger.__init__(self, db_path)
+
+__all__ = [
+    "SQLiteDB",
+
+    "export_as_json", "export_as_md"
+]
