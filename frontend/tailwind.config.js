5c5ea75838e8cb1b363b484c0516d47a787e63aa
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-07-09 03:01:30 +0530

    Fix Tailwind CSS Configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -37,6 +38,6 @@ export default {
       },
     },
   },
-  plugins: [],
-  darkMode: 'class',
+  plugins: [
+  ],
 }
